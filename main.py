a76bc7b63bd4c0a6ccc41cce878ca2dfa01f9e47
Даниил <thedeaddan@mail.ru>
2024-01-17 21:44:08 +0300

    Чистка папок и настройка распознавания

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import speech_recognition as sr
-
-def find_keyword(text):
-    if "Человек" in text and "Жук" in text:
-        print("Я нашел!")
+from modules.find import obscene_language
 
 def real_time_speech_recognition():
     recognizer = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        print("Говорите...")
-
-        try:
-            while True:
+    with sr.Microphone() as source:       
+        while True:
+            try: 
+                print("Калибровка..")
                 recognizer.adjust_for_ambient_noise(source)
-                audio = recognizer.listen(source, timeout=5)
+                print("Говорите...")
+                audio = recognizer.listen(source)
                 text = recognizer.recognize_google(audio, language="ru-RU")
                 print("Распознано:", text)
-                find_keyword(text)
-        except sr.UnknownValueError:
-            print("Не удалось распознать речь")
-        except sr.RequestError as e:
-            print(f"Ошибка сервиса распознавания речи; {e}")
+                if obscene_language.is_obsence(text):
+                    print("Маты найдены!")
+            except sr.UnknownValueError:
+                print("Не удалось распознать речь")
+            except sr.RequestError as e:
+                print(f"Ошибка сервиса распознавания речи; {e}")
 
 if __name__ == "__main__":
     real_time_speech_recognition()
--- modules/find/obscene_language.py
+++ modules/find/obscene_language.py
@@ -2,7 +2,7 @@ import io
 
 def is_obsence(word: str) :
     is_obsence_word = False
-    with io.open('./some_text.txt', encoding='utf-8') as file:
+    with io.open('modules/find/some_text.txt', encoding='utf-8') as file:
         for line in file:
             if word in line:
                 is_obsence_word = True
