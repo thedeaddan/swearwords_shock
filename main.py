1617bba0edcfae60725b34868dff23a436b1469e
Даниил <thedeaddan@mail.ru>
2024-01-17 20:06:18 +0300

    Изменен способ разбора потока аудио

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
 import speech_recognition as sr
-import pyaudio
 
 def find_keyword(text):
     if "Человек" in text and "Жук" in text:
         print("Я нашел!")
 
-def real_time_speech_recognition():
+def main():
     recognizer = sr.Recognizer()
 
     with sr.Microphone() as source:
         print("Говорите...")
-
-        # Задаем параметры аудиопотока
-        stream = p.open(format=pyaudio.paInt16,
-                        channels=1,
-                        rate=44100,
-                        input=True,
-                        frames_per_buffer=1024)
+        recognizer.adjust_for_ambient_noise(source)
 
         try:
             while True:
-                audio_chunk = stream.read(1024)
-                audio_data = recognizer.listen(audio_chunk)
-                text = recognizer.recognize_google(audio_data, language="ru-RU", show_all=False)
+                audio = recognizer.listen(source)
+                text = recognizer.recognize_google(audio, language="ru-RU")
+                print("Распознано:", text)
                 find_keyword(text)
         except sr.UnknownValueError:
             print("Не удалось распознать речь")
         except sr.RequestError as e:
-            print(f"Ошибка сервиса распознавания речи: {e}")
-        finally:
-            stream.stop_stream()
-            stream.close()
+            print(f"Ошибка сервиса распознавания речи; {e}")
 
 if __name__ == "__main__":
-    p = pyaudio.PyAudio()
-    real_time_speech_recognition()
-    p.terminate()
+    main()
