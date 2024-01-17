b848976017bed59e1bb43e334c21eb8991b56319
theascendedeber <142520708+theascendedeber@users.noreply.github.com>
2024-01-17 21:44:08 +0300

    Проверка слова на мат

--- blin/obscene_language.py
+++ blin/obscene_language.py
@@ -2,15 +2,8 @@ import io
 
 def is_obsence(word: str) :
     is_obsence_word = False
-    with io.open('./some_text.txt', encoding='utf-8') as file:
+    with io.open('./blin/some_text.txt', encoding='utf-8') as file:
         for line in file:
             if word in line:
                 is_obsence_word = True
-    if is_obsence_word == True:
-        return True
-    else:
-        return False
- 
-            
-if __name__ == '__main__':
-    print(is_obsence("матюшок"))
+    return is_obsence_word
