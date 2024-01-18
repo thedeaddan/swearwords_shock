72e7eb515711961bb203267ab0c2af61ae660563
Даниил <thedeaddan@mail.ru>
2024-01-18 20:38:12 +0300

    Распознавание речи переехало на JS

--- main.py
+++ main.py
@@ -1,50 +1,22 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
-import speech_recognition as sr
-import threading
-import modules.find as find
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-socketio = SocketIO(app)
-recognizer = sr.Recognizer()
-stop_listening_flag = False
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('stop-listening')
-def stop_listening():
-    global stop_listening_flag
-    stop_listening_flag = True
+@app.route('/receive_text', methods=['POST'])
+def receive_text():
+    text = request.form['text']
+    # Implement your logic to process received text here
+    print(f"Received text from browser: {text}")
+    if '*' in text.lower() or "кнопа" in text.lower():
+        return jsonify({'status': True}), 200
+    else:
+        return jsonify({'status': False}), 200
 
-@socketio.on('audio_stream')
-def start_listening():
-    global stop_listening_flag
-    stop_listening_flag = False
+    return '', 200
 
-    def background_thread():
-        global stop_listening_flag
-        with sr.Microphone() as source:
-            while not stop_listening_flag:
-                print(stop_listening_flag)
-                try:
-                    socketio.emit('message-from-backend', "Калибровка..")
-                    recognizer.adjust_for_ambient_noise(source)
-                    socketio.emit('message-from-backend',"Говорите...")
-                    audio = recognizer.listen(source,phrase_time_limit=3)
-                    text = recognizer.recognize_google(audio, language="ru-RU")
-                    socketio.emit('message-from-backend',f"Распознано:{text}")
-                    if find.is_obsence(text):
-                        socketio.emit('obscene-detected')
-                except sr.UnknownValueError:
-                    socketio.emit('message-from-backend',"Не удалось распознать речь")
-                except sr.RequestError as e:
-                    print(f"Ошибка сервиса распознавания речи; {e}")
-
-
-    thread = threading.Thread(target=background_thread)
-    thread.start()
-
-if __name__ == "__main__":
-    socketio.run(app, debug=True, port=3333, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug=True)
